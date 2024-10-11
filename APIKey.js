13bc91a3f59b2c8541af8e9380d71b397c611015
Hugo Vega <hugo17avid@gmail.com>
2024-10-11 15:08:27 -0700

    getting started

---
+++ APIKey.js
@@ -0,0 +1,5 @@
+import * as fal from "@fal-ai/serverless-client";
+
+fal.config({
+  credentials: "YOUR_FAL_KEY"
+});
---
+++ clientAPI.js
@@ -0,0 +1,17 @@
+import * as fal from "@fal-ai/serverless-client";
+
+const result = await fal.subscribe("fal-ai/flux/dev", {
+  input: {
+    prompt: "Extreme close-up of a single tiger eye, direct frontal view. Detailed iris and pupil. Sharp focus on eye texture and color. Natural lighting to capture authentic eye shine and depth. The word \"FLUX\" is painted over it in big, white brush strokes with visible texture."
+  },
+  logs: true,
+  onQueueUpdate: (update) => {
+    if (update.status === "IN_PROGRESS") {
+      update.logs.map((log) => log.message).forEach(console.log);
+    }
+  },
+});
+
+fal.config({
+    credentials: "YOUR_FAL_KEY"
+  });
